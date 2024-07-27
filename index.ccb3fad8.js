5c07f6c4a6862fb539897f0c483128415fb2a316
Denys209818 <deniskravcuk63@gmail.com>
2024-07-27 13:54:56 +0300

    make build

---
+++ index.ccb3fad8.js
@@ -0,0 +1,2 @@
+for(var e=document.getElementsByTagName("tbody")[0].querySelectorAll("tr"),t=0;t<e.length;t++){var l=e[t].querySelectorAll("td"),r=l.item(l.length-1),n=l[1].cloneNode(!0);r.insertAdjacentElement("beforebegin",n)}
+//# sourceMappingURL=index.ccb3fad8.js.map
